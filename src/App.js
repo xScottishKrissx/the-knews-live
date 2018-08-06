afb2d938587aa1401b39e6f32435cb0fd0daf9a5
xScottishKrissx <chrisdunne66@gmail.com>
2018-08-06 17:19:15 +0100

    Part 2 - Progress
    Managed to get user authentication working locally but when uploaded to website database was not being displayed. Still its a lot of progress and I have something to build on

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import fire from './fire.js'
+import fire, {auth, provider} from './fire.js'
 
 
 class App extends Component {
@@ -12,11 +12,34 @@ class App extends Component {
       this.state = {
         author: '',
         articleTitle:'',
-        articlesArray:[]
+        articlesArray:[],
+        user: null
       }
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.login = this.login.bind(this);
+      this.logout = this.logout.bind(this);
     }
+    logout(){
+      auth.signOut()
+      .then(() =>{
+        this.setState({
+          user: null
+        });
+      });
+  }
+
+    login(){
+        auth.signInWithPopup(provider)
+          .then((result) => {
+            const user = result.user;
+            this.setState({
+              user
+            });
+          });
+    }
+
+
 
     handleChange(e){
       this.setState({
@@ -33,7 +56,7 @@ class App extends Component {
       e.preventDefault();
       const itemRef = fire.database().ref('items');
       const item = {
-        author: this.state.author,
+        author: this.state.user.displayName || this.state.user.email,
         articleTitle: this.state.articleTitle
       }
       itemRef.push(item);
@@ -59,6 +82,12 @@ class App extends Component {
           articlesArray: newState
         })
       })
+
+      auth.onAuthStateChanged((user) => {
+        if(user){
+          this.setState({user});
+        }
+      })
     }
 
     render() {
@@ -66,8 +95,64 @@ class App extends Component {
             <div className="App">
 
                 <h1>the Knews - Live</h1>
+
+                {this.state.user ?
+                  <button onClick={this.logout}>Log Out</button>
+                  :
+                  <button onClick={this.login}>Login</button>
+                }
+
+                {this.state.user ?
+              <div>
+                  <div className='user-profile'>
+                    <h4>Hello, {this.state.user.displayName || this.state.user.email}</h4>
+                    <img src={this.state.user.photoURL} />
+                  </div>
+
+                  <div className=''>
+                    <form onSubmit={this.handleSubmit}>
+                      Author:
+                        <input type="text" name="author" placeholder="Author" onChange={this.handleChange} value={this.state.user.displayName || this.state.user.email}/>
+                      Article Title:
+                      <input type="text" name="articleTitle" placeholder="Article Title" onChange={this.handleChange} value={this.state.articleTitle}/>
+                      <button>Uploads</button>                 
+                    </form>
+                  </div>
+
+
+                  <div className=''>
+                    Display Articles
+                    <ul>
+                    {
+                      this.state.articlesArray.map((item) => {
+                          return(
+                            <li key={item.id}>
+                              <h3>{item.articleTitle}</h3>
+                              <p>
+                                by {item.author}
+                                {item.author === this.state.user.displayName || item.author === this.state.user.email ?
+                                <button onClick={() => this.handleDelete(item.id)}>Delete</button> 
+                                :
+                                null
+                              }
+                              </p>
+                              <button onClick={() => this.handleDelete(item.id)}>Delete</button>
+                            </li>
+                          )
+                      })
+                    }
+                    </ul>
+                  </div>
+                </div>
+
+                :
                 
-                <form onSubmit={this.handleSubmit}>
+                  <div className=''>
+                    <p>You must be logged in to see this content.</p>
+                  </div>
+                }
+                
+                {/* <form onSubmit={this.handleSubmit}>
                   <input type="text" name="author" placeholder="Author" onChange={this.handleChange} value={this.state.author}/>
                   <input type="text" name="articleTitle" placeholder="Article Title" onChange={this.handleChange} value={this.state.articleTitle}/>
                   <button>Uploads</button>
@@ -88,7 +173,7 @@ class App extends Component {
                       })
                     }
                   </ul>
-                </div>
+                </div> */}
 
             </div>
         );
