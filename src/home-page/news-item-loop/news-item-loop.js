751d75dc2ef4013cb838c21e8d8128ab9a98cd70
xScottishKrissx <chrisdunne66@gmail.com>
2018-08-10 10:48:19 +0100

    Reworking the news item loop

--- src/home-page/news-item-loop/news-item-loop.js
+++ src/home-page/news-item-loop/news-item-loop.js
@@ -11,79 +11,93 @@ const testData = GalleryData;
 
 console.log(testData);
 export const NewsItemLoop = () => {
-    return <NewMap />;    
+    return <MapDatabaseItems />;    
 }
 
-const NewMap = () => {
+
+class MapDatabaseItems extends React.Component{
+
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return "Hello";
+    }
+}
+
+
+
+// const NewMap = () => {
 
 
   
-    const myEffingMap = testData.map((value,key) => {
+//     const myEffingMap = testData.map((value,key) => {
 
-        // There is probably a better way of doing this...
-        const imgUrl = "https://unsplash.it/500/200?random=" + value.id;
-        ///... and this.
-        const style = {
-            backgroundImage: 'url(' + imgUrl + ')',
-            backgroundPosition: "bottom",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            height: "400px",
-            width:"100%"
-        }
+//         // There is probably a better way of doing this...
+//         const imgUrl = "https://unsplash.it/500/200?random=" + value.id;
+//         ///... and this.
+//         const style = {
+//             backgroundImage: 'url(' + imgUrl + ')',
+//             backgroundPosition: "bottom",
+//             backgroundRepeat: "no-repeat",
+//             backgroundSize: "cover",
+//             height: "400px",
+//             width:"100%"
+//         }
 
 
 
-        return (
-                <div className='news-square' key={key}>                    
-                        <Caption pageid={value.id} style={style} title={value.title} />
-                </div>
-        );
-  })
-    return myEffingMap;    
-}
+//         return (
+//                 <div className='news-square' key={key}>                    
+//                         <Caption pageid={value.id} style={style} title={value.title} />
+//                 </div>
+//         );
+//   })
+//     return myEffingMap;    
+// }
 
-class Caption extends React.Component{
+// class Caption extends React.Component{
 
-    constructor(props){
-        super(props);
-        this.showExcerpt = this.showExcerpt.bind(this);
-    }
+//     constructor(props){
+//         super(props);
+//         this.showExcerpt = this.showExcerpt.bind(this);
+//     }
 
-    showExcerpt(e){
-        e.preventDefault();
-        console.log("ShowExcerpt");
-        const thing = document.getElementsByClassName("news-item-link");
-        console.log(this.props.pageid);
-        console.log(thing);
-        console.log(thing[e.currentTarget])
+//     showExcerpt(e){
+//         e.preventDefault();
+//         console.log("ShowExcerpt");
+//         const thing = document.getElementsByClassName("news-item-link");
+//         console.log(this.props.pageid);
+//         console.log(thing);
+//         console.log(thing[e.currentTarget])
         
-    }
+//     }
     
-    render(){
-        const pageid = this.props.pageid;
-        const style = this.props.style;
-        const title = this.props.title
+//     render(){
+//         const pageid = this.props.pageid;
+//         const style = this.props.style;
+//         const title = this.props.title
 
-        return (
-            // <Link className="news-item-link" to={{pathname: '/articles/news-page/' + pageid}}>
+//         return (
+//             // <Link className="news-item-link" to={{pathname: '/articles/news-page/' + pageid}}>
 
-            <Link className="news-item-link" to={{pathname: '/filters/' + pageid}}>
-            {/* // <Link className="news-item-link" to={{pathname: pageid}}>         */}
+//             <Link className="news-item-link" to={{pathname: '/filters/' + pageid}}>
+//             {/* // <Link className="news-item-link" to={{pathname: pageid}}>         */}
             
 
-            <div style={style}>
+//             <div style={style}>
                 
-                <div className="news-item-link-text" onClick={this.showExcerpt}>
-                    <span>{title}</span>
-                    <p>Excerpt from Article</p>
-                </div>
+//                 <div className="news-item-link-text" onClick={this.showExcerpt}>
+//                     <span>{title}</span>
+//                     <p>Excerpt from Article</p>
+//                 </div>
                 
-            </div> 
+//             </div> 
 
-        </Link>
-        );
-    }
-} 
+//         </Link>
+//         );
+//     }
+// } 
 
 export default NewsItemLoop;
