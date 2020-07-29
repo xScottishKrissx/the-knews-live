053e22c997c5ba8c7fd7787cf7bedea4dd0ad547
Chris Dunne <chrisdunne66@gmail.com>
2020-07-29 10:20:02 +0100

    User Restricted Editing
    I managed to get the user restricted edit button working. Users will only have the ability to edit an article if the email they're logged in with matches the email associated with the current article.

--- src/news-page/news-page-view/article-area/article-area.js
+++ src/news-page/news-page-view/article-area/article-area.js
@@ -20,9 +20,13 @@ const ArticleArea = (props) => {
     
     //console.log("Logged in email:  " + fire.auth().currentUser.email)
     //console.log("Grab Email: " + props.email)
-
-   // const loggedInEmail = fire.auth().currentUser.email;
-    const loggedInEmail = "chrisdunne66@gmail.com"
+    console.log(fire.auth().currentUser)
+    
+    // Optional Chaining -- The ? is a javascript operator that helps account for null.
+    // In this example, loggedInEmail will work even if nobody is logged in, setting the value to null which was breaking the page previously.
+    // Isn't fully supported yet so might cause issues on non supported platforms.
+    const loggedInEmail = fire.auth().currentUser?.email;
+    //const loggedInEmail = "chrisdunne66@gmail.com"
     const articleEmail = props.email;
     return (
             
