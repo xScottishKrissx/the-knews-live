f88de1e0c7fb3abbd1b9cf5897fdec99c5fa890c
Chris Dunne <chrisdunne66@gmail.com>
2020-12-03 15:25:53 +0000

    V10 Release
    Managed to get the F5 error fixed, added the following to the .htaccess file: Options -MultiViews
    RewriteEngine On
    RewriteCond %{REQUEST_FILENAME} !-f
    RewriteRule ^ index.html [QSA,L]

--- src/deployment-message/deploymentmessage.js
+++ src/deployment-message/deploymentmessage.js
@@ -34,13 +34,16 @@ export const MessageBody = () =>{
     return(
 
         <div>
-        <h1>The Knews </h1>
-        <h3>Deployed V9.1 - Tagbar Fix - 22 October 2020</h3>
-        <h5>you can temporarily hide this message by clicking it. Refresh the page to get it back</h5>
+        <h1>theKnews </h1>
+        <h3>Deployed Version 10 - Implementing Swipe - 3rd December 2020</h3>
+        <h5>you can temporarily hide this message by clicking it. Refresh the page/click the unhide all button to get it back</h5>
         <p>Check out my blog for this in progress project where I post updates and detail what I am doing: <a href="https://christopherswebdevdiary.wordpress.com/">HERE</a></p>
         <p>Known Bug:: If you hit F5, you will get an error. Remove the /home from the url to get back.</p>
         <p>Known Bug:: Every time you load an article, you will see text flash briefly. </p>
-        <p>Known Issue: Tag Bar doesn't work properly when on one of the tag pages. It's currently being held together via the power of positive thought :)</p>
+        <p>Also, swiping is a bit buggy on desktop. I had considered disabling it for mobile but I decided to enable it on desktop because why not.</p>
+        <p>Also, there's no UI indicator for it yet, but you can swipe the cards to do various things:</p>
+        <p>Swiping Left will show you the article without needing to actually click onto the page. It's a bit basic right and not actually formatted correctly, but you get the idea.</p>
+        <p>Swiping Right will hide the card from view until you unhide it using the button at the top of the page.</p>
         </div>
     )
 }
