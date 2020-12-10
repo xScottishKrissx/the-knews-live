bb016287e13e9f23a827610032922cd4f3929714
Chris Dunne <chrisdunne66@gmail.com>
2020-12-10 17:07:03 +0000

    Working on Card Size Option Menu - 0.1

--- src/home-page/news-item-loop/custom-tile-size/custom-card-size.js
+++ src/home-page/news-item-loop/custom-tile-size/custom-card-size.js
@@ -9,11 +9,23 @@ class CustomCardSize extends React.Component{
         this.state = {
             currentStyle:"",
             switch:0,
-            size: ""
+            size: "",
+            style:{
+                bottom:""
+            },
+            showCardSizeOptionsPosition: false,
         }
-        this.changeCardSize = this.changeCardSize.bind(this); 
+        this.changeCardSize = this.changeCardSize.bind(this);
+        this.showCardSizeOptions = this.showCardSizeOptions.bind(this);  
     }
-
+    componentDidMount(){
+        console.log(this.state.showCardSizeOptionsPosition)
+        if(this.state.showCardSizeOptionsPosition === true || "true"){
+            this.setState({ style:{bottom:"0%"},})
+        }else{
+            this.setState({ style:{bottom:"-13%"},})
+        }
+    }
     changeCardSize(size){
         // console.log(size)
         localStorage.clear("myData");
@@ -23,11 +35,34 @@ class CustomCardSize extends React.Component{
         this.props.getCardSizeToParent(size);
     }
 
+    showCardSizeOptions(){
+        console.log(this.state.showCardSizeOptionsPosition)
+
+       if(this.state.showCardSizeOptionsPosition === true ){
+            console.log("Hide Options")
+            this.setState({
+                style:{bottom:"-13%"},
+                showCardSizeOptionsPosition: false
+            })
+            console.log("Should be False --> " + this.state.showCardSizeOptionsPosition)
+            
+       }else{
+            console.log("Show Options")
+            this.setState({ 
+                style:{ bottom:"0%"},
+                showCardSizeOptionsPosition: true
+             })
+             console.log("Should be True --> " + this.state.showCardSizeOptionsPosition)
+            
+       }
+    //    localStorage.setItem("cardSizeOptionsVisible", this.state.showCardSizeOptionsPosition);
+    }
+
     render(){
         return(            
-            <div className="cardControlSizeWrapper">
+            <div className="cardControlSizeWrapper" style={this.state.style}>
                 <div className="tileSizeControls" >
-                <h3>Card Size</h3>
+                <h3 onClick={()=> this.showCardSizeOptions()}>Card Size</h3>
                     <span className="controlBtns">
                         <button onClick={() => this.changeCardSize("10rem")}>S</button>
                         <button onClick={() => this.changeCardSize("260px")}>M</button>
