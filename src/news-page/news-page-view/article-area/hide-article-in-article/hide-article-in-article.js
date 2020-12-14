60a90d06e2d018a6039ea9761ecbb559150bc115
Chris Dunne <chrisdunne66@gmail.com>
2020-12-14 14:03:26 +0000

    Ability to Hide Articles from inside articles - V0.3
    Managed to get articles hidden from the article added to local storage, so now when you click on the hide button and go to home, that article along with previously hidden articles are hidden. The next issue is that the website throws an error if there is no articles have been hidden

--- src/news-page/news-page-view/article-area/hide-article-in-article/hide-article-in-article.js
+++ src/news-page/news-page-view/article-area/hide-article-in-article/hide-article-in-article.js
@@ -10,21 +10,54 @@ export class HideArticle extends React.Component{
 
 
     }
+    componentDidMount(){
+        console.log("Current Hidden Post List -> " + localStorage.getItem("hiddenPostList"));
+
+
+    }
 
     hideArticle(value){
-        console.log("Hide Article Button Pressed");
+        // console.log("Hide Article Button Pressed");
         console.log(localStorage.getItem("hiddenPostList"));
         console.log("Post Disappearing is Post:: " + value)
         console.log(this.state.postsArray)
         // document.getElementById(value).style.display = "none";
-        this.state.postsArray.push(value)
-        localStorage.setItem("hiddenPostList", this.state.postsArray);
+        // this.state.postsArray.push(value)
+
+        const localStorageHiddenPosts = localStorage.getItem("hiddenPostList");
+        const formattedPostsArray = localStorageHiddenPosts.split(',').map(Number)
+
+        console.log(formattedPostsArray)
+        formattedPostsArray.push(value)
+        console.log(formattedPostsArray)
+
+        localStorage.setItem("hiddenPostList", formattedPostsArray);
         console.log(localStorage.getItem("hiddenPostList"));
 
 
+
+        // const localStorageHiddenPosts = localStorage.getItem("hiddenPostList");
+        // const checkExist = setInterval(function() {
+        //     if (!!localStorageHiddenPosts && document.getElementById(value.id)) {
+        //     console.log("Exists!");
+        //     clearInterval(checkExist);
+        //     const formattedPostsArray = localStorageHiddenPosts.split(',').map(Number)
+            
+        //         for(var i = 0; i < formattedPostsArray.length; i++){
+        //             if(!!formattedPostsArray && formattedPostsArray[i].toString() === value.id.toString()){
+        //                 // console.log("Hidden Post Identified")
+        //                 // document.getElementById(value.id).style.display = "none";
+        //                 console.log("Success: " + value.id + " hidden");
+        //                 console.log(formattedPostsArray[i]);
+        //             }
+        //         }        
+
+        //     }
+        // }, 100); // check every 100ms
     }
     render(){
-        console.log("Hide Article -> " + this.props.articleId)
+        
+
         return(
             <div>
                 <button onClick={()=> this.hideArticle(this.props.articleId)}>Hide Article</button>
