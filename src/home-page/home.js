e962880d32c225d88c8be4f44767663cdef0c706
Chris Dunne <chrisdunne66@gmail.com>
2021-04-02 15:30:20 +0100

    Handling All Articles Being Hidden - V9.2.3
    Rearranged structure of code. Placed the initial database call higer up the render order. This allows me to more flexibility and reliability when rendering elements. I should no longer need to load the news-item-loop component to get items.This means I can now go direct to the tags pages(where i couldnt before).

--- src/home-page/home.js
+++ src/home-page/home.js
@@ -5,14 +5,15 @@ import './home.css';
 
 import NewsItemLoop from './news-item-loop/news-item-loop.js'
 export class Home extends React.Component{
-
     render(){
         return(
             <div className='home-wrapper' >                
                 
                 <div className="news-item-wrapper">
                     <ClearCache />
-                    <NewsItemLoop />        
+                    <NewsItemLoop 
+                        databaseProp={this.props.databaseProp}
+                        leftoverArticles={this.props.leftoverArticles} />  
                 </div>
             </div>
         )
--- src/tags/tagsv2.js
+++ src/tags/tagsv2.js
@@ -52,6 +52,7 @@ class Tags extends React.Component{
 
     componentDidMount(){
         // console.log(this.state.articlesArray)
+        console.log(this.props.databaseProp)
 
         console.log("Search Database For:: " + this.state.searchDBFor)
 
@@ -70,7 +71,8 @@ class Tags extends React.Component{
         if(JSON.parse(localStorage.getItem("editedArticleArray")) === null){
             console.log("handle null")
         }
-        const combinedEditedArrayFromStorage = JSON.parse(localStorage.getItem("editedArticleArray")).concat(JSON.parse(localStorage.getItem("editedLeftoverArticlesArray")));
+        const combinedEditedArrayFromStorage = this.props.databaseProp || JSON.parse(localStorage.getItem("editedArticleArray")).concat(JSON.parse(localStorage.getItem("editedLeftoverArticlesArray")));
+
         console.log(combinedEditedArrayFromStorage)
 
 
@@ -113,7 +115,7 @@ class Tags extends React.Component{
 
                     <CheckCache id={value.id}/>
 
-                    <HideArticle articleId={value.id}/>    
+                    <HideArticle articleId={value.id} arrayFromDatabase={this.props.databaseProp} leftoverArticles={this.props.leftoverArticles}/>    
 
                     <SwipeableList threshold= {0.25} swipeStartThreshold={1}>
                         <SwipeableListItem 
