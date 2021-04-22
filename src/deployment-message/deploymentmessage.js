87f29fafb57afd1af848cf7972298c46bb217357
Chris Dunne <chrisdunne66@gmail.com>
2021-04-22 17:34:28 +0100

    Release - V11.5.3 - Bug Update for Portfolio

--- src/deployment-message/deploymentmessage.js
+++ src/deployment-message/deploymentmessage.js
@@ -35,11 +35,13 @@ export const MessageBody = () =>{
 
         <div>
         <h1>theKnews </h1>
-        <h3>Deployed Version 11.5.2 - 22/4/2021</h3>
-        <h5>you can temporarily hide this message by clicking it. Refresh the page/click the unhide all button to get it back</h5>
+        <h3>Deployed Version 11.5.3 - 22/4/2021</h3>
+        <h5>you can temporarily hide this message by pressing it. Refresh the page/click the unhide all button to get it back</h5>
         <p>Check out my blog for this in progress project where I post updates and detail what I am doing: <a href="https://christopherswebdevdiary.wordpress.com/">HERE</a></p>
-        
-
+        <p>I am currently in the process of (in a very backwards step) writing a full blog post and release notes for the portfolio page.</p>
+        <p>However, there are some bugs in this released version and I will be updating this list as I catch/solve them</p>
+        <p>KNOWN ISSUE - If there are 2 or less articles on the page when you attempt to reload, you may get an error. Resetting the page using the button at the very top right will "fix" this and return the website to it's default state.</p>
+        <p>KNOWN ISSUE - Changing card size and then attempting to hide articles is currently not working </p>
         </div>
     )
 }
