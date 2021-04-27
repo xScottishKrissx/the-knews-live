e4fa30726857cd3e5391c3b6b7844e4942797c15
Chris Dunne <chrisdunne66@gmail.com>
2021-04-27 12:18:32 +0100

    V12.1.1 - Getting Started with Options Menu
    Added a new OptionsMenu component and a basic skeleton for a menu popup

---
+++ src/utility_components/optionsMenu/optionsMenu.js
@@ -0,0 +1,43 @@
+import React,{Component} from 'react';
+
+import "../optionsMenu/optionsMenu.css";
+
+class OptionsMenu extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            optionsMenuOpen:false
+        }
+    }
+
+    toggleMenu(){
+        console.log("Toggle Menu")
+        if(this.state.optionsMenuOpen === false){
+            document.getElementById("optionsMenuPopup" ).style.visibility = "visible"; 
+            this.setState({optionsMenuOpen:true})
+        }else{
+            document.getElementById("optionsMenuPopup" ).style.visibility = "hidden"; 
+            this.setState({optionsMenuOpen:false})
+        }
+    }
+    render(){
+        return (
+            <div id="optionsMenuWrapper">
+                <span onClick={()=> this.toggleMenu()} id="optionsMenuIcon" className="material-icons">settings</span>
+
+                <div id="optionsMenuPopup">
+                    <div>
+                        <ul>
+                            <li>Option 1</li>
+                            <li>Option 2</li>
+                            <li>Option 3</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default OptionsMenu;
