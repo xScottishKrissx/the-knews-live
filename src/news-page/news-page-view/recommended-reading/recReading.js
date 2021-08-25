38b725e8eb1d9c9b7cc05226f4e1ea866c2c1bdb
Chris Dunne <chrisdunne66@gmail.com>
2021-08-25 13:57:36 +0100

    12.10.7.8 - Recommended Reading Tweak
    Added a "author bio" section to the recommended reading section.  I think this helps make the area look less empty. I've also improved the actual recommended reading tiles through the addition of the new bio section.

--- src/news-page/news-page-view/recommended-reading/recReading.js
+++ src/news-page/news-page-view/recommended-reading/recReading.js
@@ -13,15 +13,17 @@ export const RecReading = (props) =>{
     var articleSelection = []
 
     // Random Articles
-    for(var i = 0; i < 5; i++){
+    if(articleSelection.length === 4)console.log("Break Loop")
+    for(var i = 0; i < 4; i++){
         var randomNumber = Math.floor(Math.random() * 9) + 1; 
-        console.log(randomNumber)
+        // console.log(randomNumber)
         
         var getArticles = database[randomNumber]      
-        console.log(getArticles)      
+        // console.log(getArticles)      
         
         articleSelection.push(getArticles)
-        console.log(articleSelection)
+        // console.log(articleSelection)
+        console.log(i)
     }
 
     // Precise Controls
@@ -32,8 +34,9 @@ export const RecReading = (props) =>{
         // }
 
     // Format the articles
+    console.log(articleSelection)
     const recReadingItems = articleSelection.map((value) => {
-        console.log(value.id)
+        // console.log(value.id)
         const imgUrl = "https://unsplash.it/190/108?random=" + value.id;
         const style = {
             backgroundImage: 'url(' + imgUrl + ')',
@@ -69,12 +72,61 @@ export const RecReading = (props) =>{
             
         )
     })
-
+    const getCurrentArticle = database.filter(x => x.id == props.id)
     
+    const articleInfo = getCurrentArticle.map((value) => {
+        var formatAuthor = value.author.toLowerCase().replace(/\w\s/g, '')
+        
+        return(
+            <div className="articleInfo">
+
+                <div id="articleInfo_icon_author">
+                    <span><i class="bi bi-image"></i></span>
+                    <h3>{value.author}</h3>
+                </div>
+
+                <div className="articleInfoSocial">
+
+                    <div title={"@"+ formatAuthor}>
+                        <i class="bi bi-twitter"></i>
+                        <span>{"@"+ formatAuthor}</span>
+                    </div>
+
+                    <div title={"@"+ formatAuthor}>
+                        <i class="bi bi-instagram"></i>
+                        <span>{"@"+ formatAuthor}</span>
+                    </div>
+
+                    <div title={formatAuthor + "@yahoo.com"}>
+                        <i class="bi bi-envelope-fill"></i> 
+                        <span> {formatAuthor + "@yahoo.com"}</span>
+                    </div>
+                
+                    <div title={formatAuthor + ".com"}>
+                        <i class="bi bi-globe"></i>
+                        <span>{formatAuthor + ".com"} </span>
+                    </div>
+
+                </div>
+
+                <div className="articleInfoAbout">
+                    <h3>About</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam auctor quam tortor, eu dapibus ex accumsan eget. Curabitur ut felis commodo, dignissim leo id, aliquam velit. Sed vitae aliquam nibh, eget eleifend quam. Integer volutpat ligula nec massa pellentesque, at commodo diam viverra. Quisque dolor erat, accumsan id turpis tristique, posuere auctor nunc. Fusce in auctor ligula. Donec vestibulum tincidunt eleifend.</p>
+                    {/* <p>Main Category: {value.tag}</p> */}
+                </div>
+
+            </div>
+        )
+    })
+
+    console.log(props.id)
         return(
                <div className="recReadingWrapper" style={setRandomColour}>
-                   <h2>Recommended Reading</h2>
+                    {articleInfo}
+                <div className="recReadingContainer">
+                    <h3>Recommended Reading</h3>
                    <div className="recReadingItemsWrapper">{recReadingItems}</div>
+                </div>
                    
                 </div>
         )
